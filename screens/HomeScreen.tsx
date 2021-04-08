b67d0fec5d9b9e29cf1abda7f4bc4ba4b2b15ef0
Carl Hidestål <43174680+carlhide@users.noreply.github.com>
2021-04-08 16:18:09 +0200

    style nav-tabs

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,36 +5,63 @@ import ProgressBar from "../components/ProgressBar";
 import BackgroundImage from "../components/BackgroundImage";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function HomeScreen({ }) {
+export default function HomeScreen({}) {
   return (
-      <View style={styles.container}>
-        <BackgroundImage imgPath={require("../assets/pexels-roberto-nickson-2486168.jpg")}>
-          <GlassSheet color="dark">
-            <ProgressBar
-              goal={7}
-              amountDone={2}
-              color="#2089DC"
-              title="Stretch"
-              time="daily"
-              icon="heartbeat"
-            />
-            <ProgressBar
-              goal={10}
-              amountDone={21}
-              color="#2089DC"
-              title="Workout"
-              time="daily"
-              icon="heartbeat"
-            />
-            <ProgressBar
-              goal={15}
-              amountDone={4}
-              color="#2089DC"
-              title="Eat healthy"
-              time="monthly"
-              icon="heartbeat"
-            />
-          </GlassSheet>
+    <View style={styles.container}>
+      <BackgroundImage
+        imgPath={require("../assets/pexels-roberto-nickson-2486168.jpg")}
+      >
+        <GlassSheet color="dark">
+          <ProgressBar
+            goal={7}
+            amountDone={2}
+            color="#2089DC"
+            title="Stretch"
+            time="daily"
+            icon="heartbeat"
+          />
+          <ProgressBar
+            goal={10}
+            amountDone={21}
+            color="#2089DC"
+            title="Workout"
+            time="daily"
+            icon="heartbeat"
+          />
+          <ProgressBar
+            goal={15}
+            amountDone={4}
+            color="#2089DC"
+            title="Eat healthy"
+            time="monthly"
+            icon="heartbeat"
+          />
+
+          <ProgressBar
+            goal={15}
+            amountDone={4}
+            color="#2089DC"
+            title="Eat healthy"
+            time="monthly"
+            icon="heartbeat"
+          />
+        </GlassSheet>
+      </BackgroundImage>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+});
+
+/**
+ *  
           <GlassSheet color="green">
             <Text style={{ margin: 10, backgroundColor: "transparent" }}>
               {" "}
@@ -47,16 +74,5 @@ export default function HomeScreen({ }) {
               If you see this red box you've probably done something wrong!{" "}
             </Text>
           </GlassSheet>
-        </BackgroundImage>
-      </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+ */
